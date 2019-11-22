492b838d416ef72cd4310d82c516677c5d52c8b0
dannguyencoder <dannguyencoder@gmail.com>
2019-11-22 17:19:44 +0700

    menu desktop and responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
-/*Slider demo*/
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+/* ==== nav mobile */
+const hamburger = document.querySelector('#hamburger');
+const navMobile = document.querySelector('#nav-mobile');
+console.log(navMobile);
+const nav = navMobile.querySelector('nav');
+hamburger.addEventListener('click', function() {
+    nav.classList.add('slideInLeft');
+    nav.classList.remove('slideOutLeft');
+
+    navMobile.classList.remove('hidden');
+});
+
+const overlay = navMobile.querySelector('.overlay');
+overlay.addEventListener('click', function () {
+    nav.classList.remove('slideInLeft');
+    nav.classList.add('slideOutLeft');
+
+    setTimeout(function () {
+        navMobile.classList.add('hidden');
+    }, 1000);
 });
